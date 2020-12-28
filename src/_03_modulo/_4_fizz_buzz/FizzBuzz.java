415a81f87175b263af786fc0c774969a067531c7
root <root@c7bd8a0a4aa6>
2020-12-28 20:33:36 +0000

    FizzBuzz

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -21,6 +21,28 @@ package _03_modulo._4_fizz_buzz;
 **/
 
 public class FizzBuzz {
-
+public static void main(String[] args) {
+	for(int i = 1; i<21;i++) {
+		if((i%3 == 0) & (i%5 == 0)) {
+			System.out.println("Fizzbuzz");
+		}
+		else if(i%3==0) {
+			System.out.println("Fizz");
+		}
+		else if(i%5==0) {
+			System.out.println("Buzz");
+		}
+		else {
+			System.out.println(i);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+}
 }
 
--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -9,16 +9,28 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String answer = JOptionPane.showInputDialog("Do you like bananas?");
 		//2. if they say no, 
+		if(answer.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "You are crazy!");
+		}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
+		else if(answer.equalsIgnoreCase("yes")) {
+			
+			JOptionPane.showMessageDialog(null, JOptionPane.showInputDialog("What is your favorite hobby?")+" is much better with bananas!");
+		}
+		
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
 		//4. OPTIONAL: if they say something other than "yes?" or "no?"
 		//	show a pop up that says "You are bananas!?"
-	
+		else {
+			JOptionPane.showMessageDialog(null, "You are bananas!?");
+		}
+		
 	}
 
 }
